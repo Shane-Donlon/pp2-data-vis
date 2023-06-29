f8bce84f95bb2e85132de85638611516791f41f3
Shane-Donlon <shane.donlon@live.com>
2023-06-29 11:19:59 +0100

    Refactor Script: main function create, and moviesbyyear chart move to seperate function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -18,51 +18,57 @@ function parseData(data) {
 const { csv, group, flatGroup } = d3;
 
 // barChart
-const barChart = async () => {
-  const bardata = await csv(csvUrl, parseData);
+const main = async () => {
+  const data = await csv(csvUrl, parseData);
   //   group movies by year in array form then sort smallest to largest
-  let moviesByYear = flatGroup(bardata, (d) => d.release_date).sort();
-  //   ChartJS Canvas for chart
-  let barChartArea = document
-    .querySelector("#moviesByYearChart")
-    .getContext("2d");
 
-  let dataForMoviesByYearChart = moviesByYear.map((movie, index) => {
-    let movieData = {};
-    movieData.year = {};
-    movieData.count = {};
-    movieData.year = movie[0];
-    movieData.count = movie[1].length;
-    return movieData;
-  });
-  let moviesReleasedChart = new Chart(barChartArea, {
-    type: "bar",
-    data: {
-      labels: dataForMoviesByYearChart.map((d) => d.year),
-      datasets: [
-        {
-          label: "Movies Released",
-          data: dataForMoviesByYearChart.map((d) => d.count),
-        },
-      ],
-    },
-    options: {
-      plugins: {
-        title: {
-          display: true,
-          text: "Movies by Year all Years",
-        },
+  // bar chart movies by Year
+  function moviesByYearChart() {
+    // Data for Bar Chart on load
+    let moviesByYear = flatGroup(data, (d) => d.release_date).sort();
+    //   ChartJS Canvas for chart
+    let barChartArea = document
+      .querySelector("#moviesByYearChart")
+      .getContext("2d");
+
+    let dataForMoviesByYearChart = moviesByYear.map((movie, index) => {
+      let movieData = {};
+      movieData.year = {};
+      movieData.count = {};
+      movieData.year = movie[0];
+      movieData.count = movie[1].length;
+      return movieData;
+    });
+    let moviesReleasedChart = new Chart(barChartArea, {
+      type: "bar",
+      data: {
+        labels: dataForMoviesByYearChart.map((d) => d.year),
+        datasets: [
+          {
+            label: "Movies Released",
+            data: dataForMoviesByYearChart.map((d) => d.count),
+          },
+        ],
       },
-      indexAxis: "y",
-      scales: {
-        y: {
-          beginAtZero: true,
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: "Movies by Year all Years",
+          },
         },
+        indexAxis: "y",
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+        responsive: true,
       },
-      // FIXME: - change to true this is to keep the chart small for now
-      responsive: false,
-    },
-  });
+    });
+  }
+  // functions to call charts
+  moviesByYearChart();
 };
 
-barChart();
+main();
